b83f3d7baf57cd45abdd48914f5f19550687aa6c
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-06-16 11:38:04 -0300

    Criando repository e service para Categoria

---
+++ cursomc/src/main/java/com/aluno/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.aluno.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aluno.cursomc.domain.Categoria;
+import com.aluno.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
