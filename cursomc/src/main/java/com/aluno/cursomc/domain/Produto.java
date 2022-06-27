93f7f4d674b43b8effa5d6c43d89fbba86f3706b
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-06-27 09:03:31 -0300

    Endpoint /pedidos/{id} disponibilizado

--- cursomc/src/main/java/com/aluno/cursomc/domain/Produto.java
+++ cursomc/src/main/java/com/aluno/cursomc/domain/Produto.java
@@ -37,6 +37,7 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<>(); 
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
---
+++ cursomc/src/main/java/com/aluno/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.aluno.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aluno.cursomc.domain.Pedido;
+import com.aluno.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+	}
+}
