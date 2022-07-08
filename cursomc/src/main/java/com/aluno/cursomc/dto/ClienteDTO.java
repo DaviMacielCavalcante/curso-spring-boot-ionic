01ae8243111c2017a5be6bf0a4c3d84d26cf1e56
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-07-08 09:28:28 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- cursomc/src/main/java/com/aluno/cursomc/dto/ClienteDTO.java
+++ cursomc/src/main/java/com/aluno/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.aluno.cursomc.domain.Cliente;
+import com.aluno.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
